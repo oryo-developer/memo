51db146620d7730b293af5c041d03b2e682879ac
おりょう <oryo.developer@gmail.com>
2022-12-05 19:35:53 +0900

    Memosに関する処理を実装

---
+++ lib/controller/memos_controller.dart
@@ -0,0 +1,39 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:memo/model/memo.dart';
+import 'package:memo/service/memo_service.dart';
+
+final memosProvider = StateNotifierProvider<MemosController, List<Memo>>((ref) {
+  final memoService = ref.watch(memoServiceProvider);
+  return MemosController(memoService: memoService);
+});
+
+class MemosController extends StateNotifier<List<Memo>> {
+  MemosController({required this.memoService}) : super([]) {
+    Future(() async {
+      state = await findAll();
+    });
+
+    memoService.stream.listen((memos) {
+      state = memos;
+    });
+  }
+
+  final MemoService memoService;
+
+  Future<List<Memo>> findAll() => memoService.findAll();
+
+  void update({required String text}) {
+    final memo = Memo()
+      ..text = text
+      ..updatedAt = DateTime.now();
+
+    memoService.put(memo: memo);
+  }
+
+  void delete({required int index}) async {
+    final memos = await findAll();
+    final id = memos[index].id;
+
+    memoService.delete(id: id);
+  }
+}
