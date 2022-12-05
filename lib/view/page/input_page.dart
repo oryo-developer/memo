ffd325f3c9c075ca2ae5f87ab6aceb1303186ad8
おりょう <oryo.developer@gmail.com>
2022-12-05 20:48:54 +0900

    データベースに関する処理を適用

--- lib/view/page/input_page.dart
+++ lib/view/page/input_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:memo/controller/memos_controller.dart';
 import 'package:memo/extension/focus_node_extension.dart';
+import 'package:memo/model/memo.dart';
 
 class InputPage extends HookConsumerWidget {
   const InputPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final memo = ModalRoute.of(context)!.settings.arguments as Memo;
+
+    final memosController = ref.read(memosProvider.notifier);
+
+    final textEditingController = useTextEditingController(text: memo.text);
     final focusNode = useFocusNode();
     final doneVisible = useState(false);
 
     useEffect(() {
+      if (memo.text.isEmpty) focusNode.requestFocus();
       return focusNode.addAndRemoveListener(() {
         doneVisible.value = focusNode.hasFocus;
       });
@@ -20,7 +28,13 @@ class InputPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              memosController.delete(memo: memo);
+              Navigator.pop(context);
+            },
+          ),
           Visibility(
             visible: doneVisible.value,
             child: IconButton(
@@ -39,10 +53,12 @@ class InputPage extends HookConsumerWidget {
             borderRadius: BorderRadius.circular(10),
           ),
           child: TextField(
+            controller: textEditingController,
             focusNode: focusNode,
             keyboardType: TextInputType.multiline,
             maxLines: null,
             style: Theme.of(context).textTheme.bodyMedium,
+            onChanged: (text) => memosController.update(memo: memo, text: text),
             decoration: const InputDecoration(
               contentPadding: EdgeInsets.all(8),
               border: InputBorder.none,
